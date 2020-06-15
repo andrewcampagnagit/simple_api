5d577d82f7333a99c88860c854ea4f0d04311f4c
Andrew Campagna <andrew.campagna@Andrews-MacBook-Pro.local>
2020-06-14 23:55:09 -0500

    added env route

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
 	return "Hello new-app!"
+
+
+@app.route("/env")
+def env_page():
+	return os.environ["MESSAGE"]
+	
 
 if __name__=="__main__":
 	app.run(host="0.0.0.0", port=8080)
